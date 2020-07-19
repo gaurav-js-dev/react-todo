ff6f96a8b2932b5f0c480d880186d849f8625f3d
Gaurav Sharma <gsharma.jai@salucro.com>
2020-07-19 15:00:00 +0530

    Updates

---
+++ precache-manifest.21b9f68d7c345fdf5e7a8d32de9ceb7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7669e6f0f47c7ffe1251d51c78cbd0b1",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "54cb27b7dcce3a8282a3",
+    "url": "/react-todo/static/css/main.b7c23e37.chunk.css"
+  },
+  {
+    "revision": "327d4a61f6e7e422054a",
+    "url": "/react-todo/static/js/2.08542ed9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.08542ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54cb27b7dcce3a8282a3",
+    "url": "/react-todo/static/js/main.99656762.chunk.js"
+  },
+  {
+    "revision": "d418538d4867acdd4faf",
+    "url": "/react-todo/static/js/runtime-main.1e50a7be.js"
+  }
+]);
